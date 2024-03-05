b5c3f2355dc849cceac5cd89f626b5fd1715fa51
Jose <142101502+JoseMartinezDEVWEB@users.noreply.github.com>
2024-03-05 11:02:02 -0400

    primer commit.

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+
+import Footer from "./Footer";
+import Header from "./Header";
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <div>{children}</div>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
